10408e6e5b8bfca3e0bb4fbbbdfc4610dbd1016a
datetime2 <kozo.wo1aspx@gmail.com>
2016-12-14 18:45:25 +0800

    todo

---
+++ Task.Schedu.Model/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task.Schedu.Model
+{
+    public class Users
+    {
+        public string UserId { get; set; }
+        public string PassWord { get; set; }
+        public string PassSalt { get; set; }
+        public string RealName { get; set; }
+        public bool Status { get; set; }
+        public DateTime CreateOn { get; set; }
+        public DateTime? ModifyOn { get; set; }
+        public string Remark { get; set; }
+        public string LastLogIP { get; set; }
+        public DateTime LastLogTine { get; set; }
+    }
+}
--- Task.Schedu.Web/Modules/UserModule.cs
+++ Task.Schedu.Web/Modules/UserModule.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Nancy;
+using Nancy.ModelBinding;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Task.Schedu.Model;
+using Task.Schedu.User;
 
 namespace Task.Schedu.Web.Modules
 {
@@ -10,6 +14,13 @@ namespace Task.Schedu.Web.Modules
         public UserModule() : base("User")
         {
             Get["/"] = x => View["Grid"];
+            Get["/Edit"] = x => View["Edit"];
+            //列表查询接口
+            Post["/PostQuery"] = r =>
+            {
+                QueryCondition condition = this.Bind<QueryCondition>();
+                return Response.AsJson(UserHelper.Query(condition));
+            };
         }
     }
 }
